a311bc18413f46057f212ff56b5a59a9af2884dd
Gustavo <eugumatos@Gustavos-MacBook-Air.local>
2020-07-16 23:35:33 -0300

    First commit backend

---
+++ Backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '99a0806e6d951079f23910baf1685e3b',
+    expiresIn: '1d',
+  },
+};
---
+++ Backend/src/models/Appointment.ts
@@ -0,0 +1,29 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import User from './User';
+
+// KISS - Keep It Simple & Stupid
+
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
+
+  @Column('timestamp with time zone')
+  date: Date;
+}
+
+export default Appointment;
