efc9474908c97ca923fb179ef42aa8095e0ffa3d
Gustavo <eugumatos@Gustavos-MacBook-Air.local>
2021-03-01 20:12:17 -0300

    Apply dependency inversion

--- Backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ Backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,29 +1,33 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
-import AppointmentRepository from '@modules/appointments/repositories/AppointmentRepository';
+import AppointmentRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentsService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRoute = Router();
+const appointmentsRepository = new AppointmentRepository();
 
 appointmentsRoute.use(ensureAuthenticated);
 
+/*
 appointmentsRoute.get('/', async (req, res) => {
   const appointmentRepository = getCustomRepository(AppointmentRepository);
   const appointments = await appointmentRepository.find();
 
   return res.json(appointments);
 });
+*/
 
 appointmentsRoute.post('/', async (req, res) => {
   const { provider_id, date } = req.body;
 
   const parseDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
 
   const appointment = await createAppointment.execute({
     date: parseDate,
