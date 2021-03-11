bbd8d47bddc0e1c3eba3740641a72556dbb368f9
Gustavo <eugumatos@Gustavos-MacBook-Air.local>
2021-03-11 20:56:46 -0300

    Finished reset password

--- Backend/src/modules/users/services/ResetPasswordService.spec.ts
+++ Backend/src/modules/users/services/ResetPasswordService.spec.ts
@@ -1,21 +1,25 @@
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeUserTokenRepository from '../repositories/fakes/FakeUserTokensRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import ResetPasswordService from './ResetPasswordService';
 
 let fakeUsersRepository: FakeUsersRepository;
 let fakeUserTokensRepository: FakeUserTokenRepository;
-let resetPasswordService: ResetPasswordService;
+let fakeHashProvider: FakeHashProvider;
+let resetPassword: ResetPasswordService;
 
 describe('SendForgotPasswordEmail', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepository();
     fakeUserTokensRepository = new FakeUserTokenRepository();
+    fakeHashProvider = new FakeHashProvider();
 
-    resetPasswordService = new ResetPasswordService(
+    resetPassword = new ResetPasswordService(
       fakeUsersRepository,
       fakeUserTokensRepository,
+      fakeHashProvider,
     );
   });
 
@@ -28,13 +32,61 @@ describe('SendForgotPasswordEmail', () => {
 
     const { token } = await fakeUserTokensRepository.generate(user.id);
 
-    await resetPasswordService.execute({
+    const generateHash = jest.spyOn(fakeHashProvider, 'generateHash');
+
+    await resetPassword.execute({
       password: '12345',
       token,
     });
 
     const updatedUser = await fakeUsersRepository.findById(user.id);
 
+    expect(generateHash).toHaveBeenCalledWith('12345');
     expect(updatedUser?.password).toBe('12345');
+  });
+
+  it('should not be able to reset the password with non-existing token', async () => {
+    await expect(
+      resetPassword.execute({
+        token: 'non-existing-token',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to reset the password with non-existing user', async () => {
+    const { token } = await fakeUserTokensRepository.generate(
+      'non-existing-user',
+    );
+
+    await expect(
+      resetPassword.execute({
+        token,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to reset password if passed more than 2 hours', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Jonh Doe',
+      email: 'jonhdoe@example.com',
+      password: '123456',
+    });
+
+    const { token } = await fakeUserTokensRepository.generate(user.id);
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      const customDate = new Date();
+
+      return customDate.setHours(customDate.getHours() + 3);
+    });
+
+    await expect(
+      resetPassword.execute({
+        password: '12345',
+        token,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
---
+++ Backend/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -0,0 +1,35 @@
+import nodemailer, { Transporter } from 'nodemailer';
+
+import IMailProvider from '../models/IMailProvider';
+
+export default class EtherealMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor() {
+    nodemailer.createTestAccount().then((account) => {
+      const transporter = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure, // true for 465, false for other ports
+        auth: {
+          user: account.user, // generated ethereal user
+          pass: account.pass, // generated ethereal password
+        },
+      });
+
+      this.client = transporter;
+    });
+  }
+
+  public async sendMail(to: string, body: string): Promise<void> {
+    const message = await this.client.sendMail({
+      from: 'Equipe GoBarber <equipe@gobarber.com.br>',
+      to,
+      subject: 'Recuperação de senha',
+      text: body,
+    });
+
+    console.log('Message Id', message.messageId);
+    console.log('Preview message', nodemailer.getTestMessageUrl(message));
+  }
+}
